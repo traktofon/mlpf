94a802750a62a324a242e45bcbaa4e92c77fd3ce
Frank Otto <Frank.Otto@pci.uni-heidelberg.de>
2013-02-13 16:55:31 +0100

    add stopcode

--- core/base.f90
+++ core/base.f90
@@ -5,4 +5,12 @@ module base
 
    integer,parameter :: dbl = selected_real_kind(15,300) ! IEEE754 double precision
 
+   contains
+
+   subroutine stopnow(msg)
+      character(len=*),intent(in) :: msg
+      write (*,'(2a)') "FATAL: ", msg
+      stop 1
+   end subroutine stopnow
+
 end module base
