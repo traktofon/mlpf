373f04053ff5ed6535c35057f883631ec9ea67e5
Frank Otto <16932347+traktofon@users.noreply.github.com>
2013-03-25 13:38:22 +0100

    update dependencies

--- Deps.mk
+++ Deps.mk
@@ -4,6 +4,8 @@ obj/dof_io_m.o obj/dof_io_m.mod: core/dof_io_m.f90 obj/dof_m.mod \
 obj/dof_m.o obj/dof_m.mod: core/dof_m.f90 obj/base_m.mod obj/tokenize_m.mod
 obj/dvr_exp_m.o obj/dvr_exp_m.mod: core/dvr_exp_m.f90 obj/dvr_m.mod \
  obj/dof_m.mod obj/tokenize_m.mod obj/units_m.mod obj/base_m.mod
+obj/dvr_fft_m.o obj/dvr_fft_m.mod: core/dvr_fft_m.f90 obj/dvr_m.mod \
+ obj/dof_m.mod obj/tokenize_m.mod obj/dvr_exp_m.mod obj/base_m.mod
 obj/dvr_ho_m.o obj/dvr_ho_m.mod: core/dvr_ho_m.f90 obj/dvr_m.mod \
  obj/dof_m.mod obj/tokenize_m.mod obj/units_m.mod obj/base_m.mod
 obj/dvr_leg_m.o obj/dvr_leg_m.mod: core/dvr_leg_m.f90 obj/dvr_m.mod \
@@ -11,6 +13,7 @@ obj/dvr_leg_m.o obj/dvr_leg_m.mod: core/dvr_leg_m.f90 obj/dvr_m.mod \
 obj/dvr_m.o obj/dvr_m.mod: core/dvr_m.f90 obj/dof_m.mod obj/base_m.mod
 obj/dvr_sin_m.o obj/dvr_sin_m.mod: core/dvr_sin_m.f90 obj/dvr_m.mod \
  obj/dof_m.mod obj/tokenize_m.mod obj/units_m.mod obj/base_m.mod
+obj/fileutil_m.o obj/fileutil_m.mod: core/fileutil_m.f90 obj/base_m.mod
 obj/genpot_m.o obj/genpot_m.mod: core/genpot_m.f90 obj/base_m.mod \
  obj/logging_m.mod obj/dof_m.mod obj/dof_io_m.mod obj/mmap_m.mod \
  obj/base_m.mod
@@ -27,8 +30,8 @@ obj/map_str2dbl_m.o obj/map_str2dbl_m.mod: core/map_str2dbl_m.f90 \
 obj/map_str2int_m.o obj/map_str2int_m.mod: core/map_str2int_m.f90 \
  obj/strutil_m.mod
 obj/meta_dof_m.o obj/meta_dof_m.mod: core/meta_dof_m.f90 obj/dvr_ho_m.mod \
- obj/dvr_leg_m.mod obj/dvr_sin_m.mod obj/dvr_exp_m.mod obj/dof_m.mod \
- obj/base_m.mod
+ obj/dvr_leg_m.mod obj/dvr_sin_m.mod obj/dvr_fft_m.mod obj/dvr_exp_m.mod \
+ obj/dof_m.mod obj/base_m.mod
 obj/mmap_m.o obj/mmap_m.mod: core/mmap_m.f90 obj/base_m.mod
 obj/modeutil_m.o obj/modeutil_m.mod: core/modeutil_m.f90
 obj/parse_pbasis_m.o obj/parse_pbasis_m.mod: core/parse_pbasis_m.f90 \
@@ -54,4 +57,4 @@ obj/mlpf.o: main/mlpf.f90 obj/tokenize_m.mod obj/parse_run_m.mod \
  obj/parse_pot_m.mod obj/parse_pbasis_m.mod obj/parse_tree_m.mod \
  obj/inp_tree_m.mod obj/tree_m.mod obj/runopts_m.mod obj/meta_dof_m.mod \
  obj/dof_io_m.mod obj/genpot_m.mod obj/hiertuck_m.mod obj/graphviz_m.mod \
- obj/units_m.mod obj/strutil_m.mod obj/base_m.mod
+ obj/units_m.mod obj/strutil_m.mod obj/fileutil_m.mod obj/base_m.mod
